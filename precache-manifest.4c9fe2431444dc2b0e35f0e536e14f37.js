0d3c893a2e4a2ba55b57f3ff6e3ce373f6da5c5f
Dastankin <hoaivong123@gmail.com>
2019-04-09 08:51:43 +0700

    Update
    Update

---
+++ precache-manifest.4c9fe2431444dc2b0e35f0e536e14f37.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e401db1402b22c39edf1f003d928a0e",
+    "url": "/MySpotify/static/media/tumblr.4e401db1.gif"
+  },
+  {
+    "revision": "4a8ede3e6f7e9eb7ce0f0e83af186fb3",
+    "url": "/MySpotify/static/media/test.4a8ede3e.jpg"
+  },
+  {
+    "revision": "17eb45df63498296d9e79ac70770c32e",
+    "url": "/MySpotify/static/media/cover-small.17eb45df.jpeg"
+  },
+  {
+    "revision": "f638c85842ac980db162",
+    "url": "/MySpotify/static/js/runtime~main.f638c858.js"
+  },
+  {
+    "revision": "fa9ca28a019eb8a728e1",
+    "url": "/MySpotify/static/js/main.fa9ca28a.chunk.js"
+  },
+  {
+    "revision": "7a16242ee5dd90643022",
+    "url": "/MySpotify/static/js/2.7a16242e.chunk.js"
+  },
+  {
+    "revision": "fa9ca28a019eb8a728e1",
+    "url": "/MySpotify/static/css/main.2550e609.chunk.css"
+  },
+  {
+    "revision": "fe1925ac18b6f503c38868a4f1584307",
+    "url": "/MySpotify/index.html"
+  }
+];
